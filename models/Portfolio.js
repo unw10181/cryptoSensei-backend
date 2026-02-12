3c4ca0a49dd7f0c089aec0e7dd09e285e32507f2
unw10181 <uchennanwilliams@gmail.com>
2026-02-12 14:01:11 -0500

    achievement time stamps

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose");
 
-const portfolioSchema = new mongoose.Schema({});
+const portfolioSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Portfolio name is required"],
+    trim: true,
+    maxlength: [50, "Portfolio name cannot exceed 50 characters"],
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: [200, "Description cannot exceed 200 characters"],
+    default: "",
+  },
+});
