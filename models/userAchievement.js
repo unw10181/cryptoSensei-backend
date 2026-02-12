8ba80ad472118bef5ae9d828a60c3c5cb0fc3e3b
unw10181 <uchennanwilliams@gmail.com>
2026-02-12 13:30:59 -0500

    userAchievements

---
+++ models/userAchievement.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userAchievementSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    achievementId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Achievement",
+      required: true,
+    },
+    unlockedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Code below is to Prevent duplicate unlocks 
+userAchievementSchema.index({ userId: 1, achievementId: 1 }, { unique: true });
+
+module.exports = mongoose.model("UserAchievement", userAchievementSchema);
