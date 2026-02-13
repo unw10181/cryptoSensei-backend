4451cda9225b5471093ad6dddc6b31fdf7281a65
unw10181 <uchennanwilliams@gmail.com>
2026-02-12 22:35:57 -0500

    custom error class

--- middleware/errMiddleware.js
+++ middleware/errMiddleware.js
@@ -3,3 +3,14 @@ const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
 
+// Custom error class
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
