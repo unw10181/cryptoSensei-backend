0d8096c6eacd02f0910d10c598d9e74ca60f38af
unw10181 <uchennanwilliams@gmail.com>
2026-02-13 10:42:20 -0500

    async handling method

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,7 @@
 const User = require("../models/User");
 const { asyncHandler, AppError } = require("../middleware/errorMiddleware");
 const { sendTokenResponse } = require("../utils/generateToken");
+
+
+const register = asyncHandler(async (req, res, next) => { 
+}
