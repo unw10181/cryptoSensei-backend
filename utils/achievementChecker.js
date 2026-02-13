f0e87df98722a4997c9cb1f63665250a75194b58
unw10181 <uchennanwilliams@gmail.com>
2026-02-12 22:56:11 -0500

    imported models

---
+++ utils/achievementChecker.js
@@ -0,0 +1,6 @@
+const Achievement = require("../models/Achievement");
+const User = require("../models/User");
+const Transaction = require("../models/Transaction");
+const Portfolio = require("../models/Portfolio");
+const UserAchievement = require("../models/userAchievement");
+
