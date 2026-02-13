614fb2fb9bf030f0dbcd5bfd27d9b4335abd298e
unw10181 <uchennanwilliams@gmail.com>
2026-02-12 22:23:27 -0500

    checking for token auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,5 +3,22 @@ const User = require('../models/User');
 
 const protect = async (req, res, next) => {
   let token;
-}
+
+  // Check for Bearer token in Authorization header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Not authorized. No token provided.",
+    });
+  }
+
+  
+};
 
