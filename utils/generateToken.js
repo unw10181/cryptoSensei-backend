c06510f86e773a7bb2830cd4d6b04efa64194a80
unw10181 <uchennanwilliams@gmail.com>
2026-02-12 22:59:14 -0500

    token generator

---
+++ utils/generateToken.js
@@ -0,0 +1,8 @@
+const jwt = require("jsonwebtoken");
+
+// Generate JWT Token 
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  });
+};
