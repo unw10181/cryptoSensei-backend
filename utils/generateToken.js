706ae4ec7a9539a425a88d3dfadff4234673e310
unw10181 <uchennanwilliams@gmail.com>
2026-02-12 23:01:04 -0500

    token response

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,8 +1,30 @@
 const jwt = require("jsonwebtoken");
 
-// Generate JWT Token 
+// Generate JWT Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN || "7d",
   });
 };
+
+// Send as token
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = generateToken(user._id);
+
+  res.status(statusCode).json({
+    success: true,
+    token,
+    user: {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      virtualBalance: user.virtualBalance,
+      rank: user.rank,
+      totalXP: user.totalXP,
+      avatar: user.avatar,
+      createdAt: user.createdAt,
+    },
+  });
+};
+
+module.exports = { generateToken, sendTokenResponse };
