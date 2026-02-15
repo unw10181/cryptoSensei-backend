f2297e431d141f467a25a68421baf5383f98d552
unw10181 <uchennanwilliams@gmail.com>
2026-02-15 14:42:55 -0500

    checkfor routes

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ const app = express();
 connectDb();
 
 // Route imports
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const portfolioRoutes = require('./routes/portfolioRoutes');
-const transactionRoutes = require('./routes/transactionRoutes');
-const cryptoRoutes = require('./routes/cryptoRoutes');
-const achievementRoutes = require('./routes/achievementRoutes');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const portfolioRoutes = require("./routes/portfolioRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+const cryptoRoutes = require("./routes/cryptoRoutes");
+const achievementRoutes = require("./routes/achievementRoutes");
+
+// Health Check
+app.get("/", (req, res) => {
+  res.json({ message: "CryptoSensei API is running. Hayaku!" });
+});
 
 app.express();
 
