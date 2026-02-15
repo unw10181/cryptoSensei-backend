852bdd03f8df65705f05acefdece6b47bdd9f910
unw10181 <uchennanwilliams@gmail.com>
2026-02-15 14:19:36 -0500

    get achievements method

--- controllers/achievementController.js
+++ controllers/achievementController.js
@@ -2,3 +2,17 @@ const Achievement = require("../models/Achievement");
 const UserAchievement = require("../models/userAchievement");
 const { asyncHandler, AppError } = require("../middleware/errorMiddleware");
 const { checkAchievements } = require("../utils/achievementChecker");
+
+
+// Get all available achievements
+// GET /api/achievements
+
+const getAllAchievements = asyncHandler(async (req, res) => {
+  const achievements = await Achievement.find().sort({ tier: 1, xpReward: 1 });
+
+  res.status(200).json({
+    success: true,
+    count: achievements.length,
+    data: achievements,
+  });
+});
